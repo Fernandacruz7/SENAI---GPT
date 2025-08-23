76e6fe05a015efc1e23d5dd863719d6aed9af6bb
Alex <alexjose1995@icloud.com>
2025-08-22 21:50:23 -0300

    contruindo  JavaScript

---
+++ assets/imgs/javascript/condicionais.js
@@ -0,0 +1,21 @@
+// console.log("Hello World!")
+
+// let nome = prompt("qual seu nome?");
+// let idade= prompt("qual sua idade?");
+
+// alert(" Meu nome e " + nome + " tenho " + idade );
+
+let idade = prompt("qual e a idade?"); 
+
+if (idade > 18) {
+
+alert("parabens! voce e maior de idade.");
+
+
+} else {
+
+alert("Vai pra super Nani!")
+
+
+}
+
