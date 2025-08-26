1e27b13bf7da13ef91fcc9cc27eb4c4a65f83269
Alex <alexjose1995@icloud.com>
2025-08-25 21:52:12 -0300

    contruindo prompt

--- assets/imgs/javascript/condicionais.js
+++ assets/imgs/javascript/condicionais.js
@@ -1,21 +1,130 @@
-// console.log("Hello World!")
+// // Exercicio 1
+// let idade = prompt("Qual a sua idade?");
 
-// let nome = prompt("qual seu nome?");
-// let idade= prompt("qual sua idade?");
+// if (idade >= 18) {
+//     if(idade >=60) {
 
-// alert(" Meu nome e " + nome + " tenho " + idade );
+//         alert("Idoso")
+//     }
+    
+//     //bloco verdadeiro
+//  alert("Voce e maior de idade!");
 
-let idade = prompt("qual e a idade?"); 
+// } else {
 
-if (idade > 18) {
+//     // bloco do falso
+// alert("Va pra super nani!");
 
-alert("parabens! voce e maior de idade.");
+// }
+
+// Exercicio 2
+//...
+ 
+// let idade = prompt("Digite um numero?");
+
+// if (idade >= 0) {
+
+// alert("o numero e positivo");
+    
+// } else {
+
+// alert("O numero e negativo");
+
+// }
 
 
-} else {
+// Exercicio 3 
+// //...
 
-alert("Vai pra super Nani!")
+// let Nota = prompt("Digite a nota do aluno");
+
+// if (Nota >= 60) {
+
+// alert("Aprovado");
+
+// } else {
+
+// alert("Reprovado");
+
+// }
+
+// Exercicio 4 
+//...
+
+// let numero = prompt("digite um numero");
+
+// if (numero > 0) {
+
+//     alert("o numero e positivo");
+
+// } else {
+
+// if (numero == 0) {
+
+//     alert("o numero e zero");
+
+// } else {
+
+// alert ("o numero e negativo");
+
+// }
+
+// Exercicio 5 
+//...
+
+// let idade = prompt("digite sua idade");
+
+// if (idade > 0 && idade < 12) {
+
+// alert("voce e uma crianca");
+
+// } else {
+
+// if (idade > 12 && idade < 17) {
+
+//     alert("voce e um adolescente");
+
+// } else {
+
+//     if (idade >= 18) {
+
+//         alert("voce e um adulto");
+//     }
+// }
+// }
+
+// Exercicio 6
+//...
+
+// let numero = prompt("digite um numero");
+
+// if (numero % 2 == 0 ) {
+
+// alert("o numero e par"); 
+
+// } else {
+
+//     alert ("o numero e impar");
+    
+
+// }
+
+// nivel intermediario Exercicio 1
+//...
+
+let numero1 = prompt("digite um numero");
+let numero2 = prompt("digite um numero");
+let operacao = prompt("digite a operacao (+, -, *, /,:");
+
+let resultado;
+
+if (operacao === "+") {
+resultado n1 + n2 ;
+ 
+} else if (operacao === "+") {
+    resultado n1 + n2 ;
 
 
 }
+
 
