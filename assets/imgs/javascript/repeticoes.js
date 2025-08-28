e0211a0a65c8cd9762e8a8418f8e273d495d50f6
Fernanda cruz <fehcruz002@gmail.com>
2025-08-27 21:56:03 -0300

    repeticoes

---
+++ assets/imgs/javascript/repeticoes.js
@@ -0,0 +1,60 @@
+// console.log("FOR")
+
+// for (let contadora = 1; contadora <= 5; contadora++); {
+
+//     console.log(contadora);
+
+// }
+
+// console.log.apply("WHILE");
+// let contadoraWhile = 1;
+// while (contadoraWhile <= 5) {
+
+//     console.log(contadoraWhile);
+//      contadoraWhile++;
+
+//  }
+
+//  debugger
+//  console.log("DO WHILE SIMPLES");
+
+//  do {
+
+//     console.log("mostrou o valor na tela, ainda que a condicao seja falsa")
+
+//  } while (1 == 2);
+
+// debugger
+// for (let contadora = 1; contadora <= 10; contadora++) {
+
+//     console.log(contadora);
+
+// }
+
+// debugger
+
+// let numero = prompt("digite um numero");
+// for (let contadora = 1; contadora <=10; contadora++) {
+
+//     let resultado = contadora * numero
+
+// console.log(resultado)
+// }
+
+ 
+// Exercicio 3
+// debugger
+
+// let N = prompt("digite um numero ");
+// N = Number(N);
+
+// let soma = 0;
+
+// for (let contadora = 1; contadora <= N; contadora++) {
+
+//     soma = soma + contadora;
+
+//     console.log(soma)
+
+
+// }
